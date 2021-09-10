51d6b810b2fe80b86726fe27291add41252ab4b1
thanasis zeliotis <open_the_mailbox@yahoo.com>
2021-09-10 17:16:38 +0300

    few new modules

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,10 @@
 import os
 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app= Flask(__name__)
+db = SQLAlchemy(app)
 
 # a simple page that renders a page
 @app.route('/')

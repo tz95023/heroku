eed9b7424e187d1698eb1c92a0354cf83df886df
tz95023 <open_the_mailbox@yahoo.com>
2021-11-26 10:46:53 +0200

    commit to server

---
+++ server/main.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+main_blueprint = Blueprint('main', __name__)
+
+@main_blueprint.route('/')
+def index():
+    return render_template('index.html')
+
+@main_blueprint.route('/profile')
+def profile():
+    return render_template('profile.html')
